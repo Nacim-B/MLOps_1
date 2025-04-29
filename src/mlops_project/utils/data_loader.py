76c9d9a89350469ba51c8b3a08c0fbe4fd693afd
Nacim <nacimberrabah@protonmail.com>
2025-04-29 16:56:49 +0200

    feat: new logic of data source

---
+++ src/mlops_project/utils/data_loader.py
@@ -0,0 +1,67 @@
+import os
+import pandas as pd
+import sqlalchemy
+from mlops_project.utils.s3_handler import S3Handler
+
+
+class DataLoader:
+    def __init__(self, config: dict):
+        """
+        Initialize the DataLoader with the project configuration.
+        """
+        self.config = config
+        self.data_source = self.config['data_source']
+        self.s3_handler = S3Handler(
+                bucket=os.getenv("S3_BUCKET_NAME"),
+                config=self.config
+            )
+
+    def load_data(self) -> pd.DataFrame:
+        """
+        Load the dataset depending on the source type.
+        """
+        if self.data_source == 'csv_url':
+            self.s3_handler.upload_csv_from_url_to_s3(
+                os.getenv("CSV_URL"),
+                os.getenv("CSV_FILENAME")
+            )
+            return self.s3_handler.load_csv_from_s3(f"datasets/{os.getenv("CSV_FILENAME")}")
+
+        elif self.data_source  == 'csv_s3':
+            return self.s3_handler.load_csv_from_s3(f"datasets/{os.getenv("CSV_FILENAME")}")
+
+        elif self.data_source  == 'mysql':
+            return self._load_from_mysql()
+        else:
+            raise ValueError(f"Unknown data source type: {self.data_source}")
+
+
+    def _load_from_mysql(self) -> pd.DataFrame:
+        """
+        Load data from a MySQL database using a SQL query from file.
+        """
+        mysql_config = self.data_source['mysql']
+
+        # Retrieve credentials from environment variables
+        username = os.getenv("MYSQL_USERNAME")
+        password = os.getenv("MYSQL_PASSWORD")
+        host = os.getenv("MYSQL_HOST")
+        port = os.getenv("MYSQL_PORT", "3306")
+        database = mysql_config.get('database') or os.getenv("MYSQL_DATABASE")
+
+        # Build SQLAlchemy connection string
+        connection_string = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
+
+        print(f"📥 Connecting to MySQL database: {database}")
+
+        engine = sqlalchemy.create_engine(connection_string)
+
+        # Load the query from file
+        query_path = self.config.get('query_file', 'queries/train_query.sql')
+        with open(query_path, "r") as file:
+            query = file.read()
+
+        print(f"📄 Executing query from {query_path}")
+        df = pd.read_sql_query(query, con=engine)
+
+        return df
